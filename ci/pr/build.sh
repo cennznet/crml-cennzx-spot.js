ad7fb9abe203ba0064c9c0f74cb41cb0571c684f
karishma <bothara.karishma@gmail.com>
2019-04-04 16:05:13 +1300

    Fix PR build

--- ci/pr/build.sh
+++ ci/pr/build.sh
@@ -3,7 +3,12 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 set -ex
 
+# Required Environment variables. Set them by using the commands below
+: "${IMAGE_NAME:?IMAGE_NAME environment variable is required}"
+: "${GEMFURY_TOKEN:?GEMFURY_TOKEN environment variable is required}"
+
 : "${IMAGE_NAME:?IMAGE_NAME is required}"
 
 docker build -t "${IMAGE_NAME}" \
-       -f $DIR/Dockerfile .
+     --build-arg GEMFURY_TOKEN="${GEMFURY_TOKEN}" \
+     -f $DIR/Dockerfile .
